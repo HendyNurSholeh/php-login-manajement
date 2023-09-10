55febc86a3483c561124f3b241339524bf669c47
Hendy Nur Sholeh <hendynursholeh7213@gmail.com>
2023-09-10 23:08:17 +0800

    implement SessionRepositoryTest.php

--- test/Repository/SessionRepositoryTest.php
+++ test/Repository/SessionRepositoryTest.php
@@ -2,6 +2,7 @@
     namespace HendyNurSholeh\Repository;
 
 use HendyNurSholeh\Config\Database;
+use HendyNurSholeh\Domain\Session;
 use PDO;
 use PHPUnit\Framework\TestCase;
     class SessionRepositoryTest extends TestCase{
@@ -13,8 +14,25 @@ use PHPUnit\Framework\TestCase;
             $this->sessionRepository->deleteAll();
         }
 
-        public function testSave(): void{
-                      
+        public function testSaveSuccess(): void{
+            $session = new Session(uniqid(), "user123");
+            $this->sessionRepository->save($session);
+            $result = $this->sessionRepository->findById($session->getId());
+            self::assertEquals($session, $result);
+            self::assertNotNull($result);
+        }
+        
+        public function testFindByIdNotFound(): void{
+            $result = $this->sessionRepository->findById("salah");
+            self::assertNull($result);
+        }
+        
+        public function testDeleteByIdSuccess(): void{
+            $session = new Session(uniqid(), "user123");
+            $this->sessionRepository->save($session);
+            $this->sessionRepository->deleteById($session->getId());
+            $result = $this->sessionRepository->findById($session->getId());
+            self::assertNull($result);
         }
     }
 ?>
