ce6a564069621e0eaa204cd78d294316d7093ed3
Hendy Nur Sholeh <hendynursholeh7213@gmail.com>
2023-09-30 23:39:56 +0800

    feature middleware : modifed class controller with middleware in 'public/index.php'

--- public/index.php
+++ public/index.php
@@ -5,14 +5,16 @@ use HendyNurSholeh\App\Router;
 use HendyNurSholeh\Config\Database;
 use HendyNurSholeh\Controller\HomeController;
 use HendyNurSholeh\Controller\UserController;
+use HendyNurSholeh\Middleware\MustLoginMiddleware;
+use HendyNurSholeh\Middleware\MustNotLoginMiddleware;
 
 Database::getConnection("prod");
 
 Router::add('GET', '/', HomeController::class, 'index');
-Router::add('GET', '/users/register', UserController::class, 'register');
-Router::add('POST', '/users/register', UserController::class, 'postRegister');
-Router::add('GET', '/users/login', UserController::class, 'login');
-Router::add('POST', '/users/login', UserController::class, 'postLogin');
-Router::add('GET', '/users/logout', UserController::class, 'logout');
+Router::add('GET', '/users/register', UserController::class, 'register', [MustNotLoginMiddleware::class]);
+Router::add('POST', '/users/register', UserController::class, 'postRegister', [MustNotLoginMiddleware::class]);
+Router::add('GET', '/users/login', UserController::class, 'login', [MustNotLoginMiddleware::class]);
+Router::add('POST', '/users/login', UserController::class, 'postLogin', [MustNotLoginMiddleware::class]);
+Router::add('GET', '/users/logout', UserController::class, 'logout', [MustLoginMiddleware::class]);
 
 Router::run();
