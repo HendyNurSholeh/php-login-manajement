93755b170fcb89d0fb3bf0fd050a6bcb5366f2f8
Hendy Nur Sholeh <hendynursholeh7213@gmail.com>
2023-10-01 21:41:50 +0800

    add model user request and response

---
+++ app/Model/UserPasswordChangeRequest.php
@@ -0,0 +1,9 @@
+<?php 
+namespace HendyNurSholeh\Model;
+class UserPasswordChangeRequest{
+    public ?string $id = null;
+    public ?string $oldPassword = null;
+    public ?string $newPassword = null;
+
+}
+?>
