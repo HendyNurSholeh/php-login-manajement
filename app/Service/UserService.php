8bc380ea6cfbc1a6875901fa7a468a9aa0066abc
Hendy Nur Sholeh <hendynursholeh7213@gmail.com>
2023-10-01 22:07:03 +0800

    implement method password in UserService.php

--- app/Service/UserService.php
+++ app/Service/UserService.php
@@ -7,6 +7,8 @@ use HendyNurSholeh\Domain\User;
 use HendyNurSholeh\Exception\ValidationException;
 use HendyNurSholeh\Model\UserLoginRequest;
 use HendyNurSholeh\Model\UserLoginResponse;
+use HendyNurSholeh\Model\UserPasswordChangeRequest;
+use HendyNurSholeh\Model\UserPasswordChangeResponse;
 use HendyNurSholeh\Model\UserProfileUpdateRequest;
 use HendyNurSholeh\Model\UserProfileUpdateResponse;
 use HendyNurSholeh\Model\UserRegisterRequest;
@@ -71,7 +73,7 @@ class UserService{
         return $response;
     }
 
-    public function validateUserLoginRequest(UserLoginRequest $request){
+    private function validateUserLoginRequest(UserLoginRequest $request){
         if(is_null($request->id) || is_null($request->password) ||
            trim($request->id) == "" || trim($request->password) == ""){
             throw new ValidationException("Id, password can't not blank");
@@ -101,12 +103,49 @@ class UserService{
         }
     }
 
-    public function validateUserProfileUpdateRequest(UserProfileUpdateRequest $request){
+    private function validateUserProfileUpdateRequest(UserProfileUpdateRequest $request){
         if(is_null($request->id) || is_null($request->username) ||
         trim($request->id) == "" || trim($request->username) == ""){
          throw new ValidationException("Id, username can't not blank");
+        }else if(strlen(trim($request->username)) < 3){
+            throw new ValidationException("username length can't be less than 3");
         }
         $request->id = strtolower($request->id);
         $request->username = strtolower($request->username);
     }
+
+    public function changePassword(UserPasswordChangeRequest $request): UserPasswordChangeResponse{
+        $this->validateUserPasswordChangeRequest($request);
+        try{
+            Database::beginTransaction();
+            $user = $this->userRepository->findById($request->id);
+            if($user==null){
+                throw new ValidationException("User is not found");
+            } else if(!password_verify($request->oldPassword, $user->getPassword())){
+                throw new ValidationException("Old Password is wrong");
+            }
+            $user->setPassword($request->newPassword);
+            $newUser = $this->userRepository->update($user);
+            $response = new UserPasswordChangeResponse();
+            $response->user = $newUser;
+            Database::commitTransaction();
+            return $response;
+        }catch (Exception $exception){
+            Database::rollbackTransaction();
+            throw $exception;
+        }
+    }
+
+    private function validateUserPasswordChangeRequest(UserPasswordChangeRequest $request){
+        if(is_null($request->id) || is_null($request->newPassword) || is_null($request->oldPassword) ||
+        trim($request->id) == "" || trim($request->newPassword) == "" || trim($request->oldPassword) == ""){
+         throw new ValidationException("Id or new password or old password can't not blank");
+        }else if(strlen(trim($request->newPassword)) < 8){
+            throw new ValidationException("password length can't be less than 8");
+        }
+        $request->id = strtolower($request->id);
+        $request->oldPassword = strtolower($request->oldPassword);
+        $request->newPassword = strtolower($request->newPassword);   
+    }
+
 }
